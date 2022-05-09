eb68978cb444627c4ea6126b45b69be2df3b7eb4
FranciscoTramontini <frantramontini97@gmail.com>
2022-05-09 19:31:06 -0300

    Model new-user hecho

---
+++ src/app/models/new-user.ts
@@ -0,0 +1,11 @@
+export class NewUser {
+    name: string;
+    nameUser: string;
+    password: string;
+
+    constructor(name: string, nameUser: string, password: string) {
+        this.name = name;
+        this.nameUser = nameUser;
+        this.password = password;
+    }
+}
