8a3a8c4a49f5df39ae198c8074ddf562d3899361
FranciscoTramontini <frantramontini97@gmail.com>
2022-05-09 13:41:59 -0300

    Agrego api Server al servicio de experiencia

--- src/app/services/experience.service.ts
+++ src/app/services/experience.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Experience } from '../models/experience';
 
 @Injectable({
@@ -8,7 +9,7 @@ import { Experience } from '../models/experience';
 })
 
 export class ExperienceService {
-  private apiServerUrl = '';
+  private apiServerUrl = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) { }
 
