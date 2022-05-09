60878854370e767480345bb47dcf17b14558e283
FranciscoTramontini <frantramontini97@gmail.com>
2022-05-09 12:38:28 -0300

    Servicio de skill hecho

--- src/app/services/skill.service.ts
+++ src/app/services/skill.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Skill } from '../models/skill';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SkillService {
 
-  constructor() { }
+export class SkillService {
+  private apiServerUrl = '';
+
+  constructor(private http: HttpClient) { }
+
+  public getSkills(): Observable<Skill[]> {
+    return this.http.get<Skill[]>(`${this.apiServerUrl}/skill/all`);
+  }
+
+  public addSkill(skill: Skill): Observable<Skill> {
+    return this.http.post<Skill>(`${this.apiServerUrl}/skill/add`, skill);
+  }
+
+  public updateSkill(skill: Skill): Observable<Skill> {
+    return this.http.put<Skill>(`${this.apiServerUrl}/skill/update`, skill);
+  }
+
+  public deleteSkill(skillId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/skill/delete/${skillId}`);
+  }
 }
