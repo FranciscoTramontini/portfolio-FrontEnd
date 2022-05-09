91d0f6cd524938c74cd31a0922857484d5fad534
FranciscoTramontini <frantramontini97@gmail.com>
2022-05-09 12:27:31 -0300

    Modelo de proyecto hecho

---
+++ src/app/models/project.ts
@@ -0,0 +1,11 @@
+export interface Project {
+    id: number;
+    name: string;
+    company: string;
+    aboutProject: string;
+    yearInc: string;
+    yearGra: string;
+    country: string;
+    status: string;
+    projectUrl: string;
+}
