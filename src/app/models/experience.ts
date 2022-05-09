ad44af2f4176a0b7b794e960d10f807a9313e506
FranciscoTramontini <frantramontini97@gmail.com>
2022-05-09 12:26:07 -0300

    Modelo de experiencia hecho

---
+++ src/app/models/experience.ts
@@ -0,0 +1,10 @@
+export interface Experience {
+    id: number;
+    company: string;
+    aboutJob: string;
+    yearInc: string;
+    yearGra: string;
+    duration: string;
+    country: string;
+    imageUrl: string;
+}
