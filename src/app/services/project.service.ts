1ca0f910f9cc073f198f635301608b4c6d6c6231
FranciscoTramontini <frantramontini97@gmail.com>
2022-05-09 12:37:14 -0300

    Servicio de proyecto hecho

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Project } from '../models/project';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProjectService {
 
-  constructor() { }
+export class ProjectService {
+  private apiServerUrl = '';
+
+  constructor(private http: HttpClient) { }
+
+  public getProjects(): Observable<Project[]> {
+    return this.http.get<Project[]>(`${this.apiServerUrl}/project/all`);
+  }
+
+  public addProject(project: Project): Observable<Project> {
+    return this.http.post<Project>(`${this.apiServerUrl}/project/add`, project);
+  }
+
+  public updateProject(project: Project): Observable<Project> {
+    return this.http.put<Project>(`${this.apiServerUrl}/project/update`, project);
+  }
+
+  public deleteProject(projectId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/project/delete/${projectId}`);
+  }
 }
