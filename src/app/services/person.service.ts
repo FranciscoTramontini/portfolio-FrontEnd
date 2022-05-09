e026d077f02b6ee165b2b33befd277c3e63a0e87
FranciscoTramontini <frantramontini97@gmail.com>
2022-05-09 12:36:10 -0300

    Servicio de persona hecho

--- src/app/services/person.service.ts
+++ src/app/services/person.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Person } from '../models/person';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PersonService {
 
-  constructor() { }
+export class PersonService {
+  private apiServerUrl = '';
+
+  constructor(private http: HttpClient) { }
+
+  public getUsers(): Observable<Person[]> {
+    return this.http.get<Person[]>(`${this.apiServerUrl}/user/all`);
+  }
+
+  public updateUser(user: Person): Observable<Person> {
+    return this.http.put<Person>(`${this.apiServerUrl}/user/update`, user);
+  }
 }
