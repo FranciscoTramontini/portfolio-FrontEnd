d95cb95464264129ccc21268241b942f4e52636c
FranciscoTramontini <frantramontini97@gmail.com>
2022-05-09 13:49:51 -0300

    Agrego los servicios al providers del app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import { SkillComponent } from './components/skill/skill.component';
 import { ProjectComponent } from './components/project/project.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { LoginComponent } from './components/login/login.component';
+import { EducationService } from './services/education.service';
+import { ExperienceService } from './services/experience.service';
+import { PersonService } from './services/person.service';
+import { ProjectService } from './services/project.service';
+import { SkillService } from './services/skill.service';
 
 @NgModule({
   declarations: [
@@ -28,7 +33,13 @@ import { LoginComponent } from './components/login/login.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    EducationService,
+    ExperienceService,
+    PersonService,
+    ProjectService,
+    SkillService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
