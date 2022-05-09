f50d85ee120788ccaf5ad14fbc0c5f6b9f4f22ca
FranciscoTramontini <frantramontini97@gmail.com>
2022-05-09 12:32:47 -0300

    Servicio de educacion hecho

--- src/app/services/education.service.ts
+++ src/app/services/education.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Education } from '../models/education';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class EducationService {
 
-  constructor() { }
+  private apiServerUrl = '';
+
+  constructor(private http: HttpClient) { }
+
+  public getEducations(): Observable<Education[]> {
+    return this.http.get<Education[]>(`${this.apiServerUrl}/education/all`);
+  }
+
+  public addEducation(education: Education): Observable<Education> {
+    return this.http.post<Education>(`${this.apiServerUrl}/education/add`, education);
+  }
+
+  public updateEducation(education: Education): Observable<Education> {
+    return this.http.put<Education>(`${this.apiServerUrl}/education/update`, education);
+  }
+
+  public deleteEducation(educationId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/education/delete/${educationId}`);
+  }
 }
