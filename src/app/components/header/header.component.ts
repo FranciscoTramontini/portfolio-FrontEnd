e2c6859c81296b4542c0ceddc036ad031bf5d69b
FranciscoTramontini <frantramontini97@gmail.com>
2022-05-10 08:42:50 -0300

    Oculto botones para header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { PersonService } from 'src/app/services/person.service';
 import { Person } from 'src/app/models/person';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-header',
@@ -12,11 +13,23 @@ import { Person } from 'src/app/models/person';
 export class HeaderComponent implements OnInit {
   public persons: Person[] = [];
   public editPerson!: Person;
+  roles: string[] = [];
+  isAdmin = false;
 
-  constructor(private userService: PersonService) { }
+  constructor(
+    private userService: PersonService,
+    private tokenService: TokenService
+  ) { }
 
   ngOnInit(): void {
     this.getPersons();
+
+    this.roles = this.tokenService.getAuthorities();
+    this.roles.forEach(role => {
+      if (role === 'ROLE_ADMIN') {
+        this.isAdmin = true;
+      }
+    });
   }
 
   public getPersons(): void {
